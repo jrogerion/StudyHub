edc76b0cfe3d61409fb703a6188f2e6a6ccc7651
jrogerion <jrogerion@gmail.com>
2025-06-06 19:49:06 -0300

    Melhorando o código

--- SeleniumAutomation/youtube_search_1.py
+++ SeleniumAutomation/youtube_search_1.py
@@ -1,26 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 ''' Código para automatizar a realização de um pesquisa simples no YOUTUBE usando o FIREFOX'''
 
 browser = webdriver.Firefox()
 browser.maximize_window()
-browser.get('https://www.youtube.com')
 
-search_box = browser.find_element(By.CSS_SELECTOR, '.ytSearchboxComponentInput')
-search_box.click()
-search_box.send_keys('atomação+selenium')
+try:
+    browser.get('https://www.youtube.com')
 
-search_button = browser.find_element(By.CSS_SELECTOR, '.ytSearchboxComponentSearchButton')
-search_button.click()
-sleep(5)
-search_box.clear()
 
-# Outra pesquisa
-search_box.click()
-search_box.send_keys('curso+python')
-search_button.click()
-sleep(5)
+    # Caixa de pesquisa
+    seach_box = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '.ytSearchboxComponentInput'))
+    ) 
+    # Botão de pesquisa
+    seach_button = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '.ytSearchboxComponentSearchButton'))
+    )
 
-browser.close()
+    # Fazendo a pesquisa
+    seach_box.send_keys('automação+selenium')
+    seach_button.click()
+    sleep(5)
+
+    # Limpando caixa de pesquisa
+    seach_box.clear()
+
+    # Realizando outra pesquisa
+    seach_box.send_keys('curso+python')
+    seach_button.click()
+    sleep(5)
+
+# # Outra pesquisa
+# search_box.click()
+# search_box.send_keys('curso+python')
+# search_button.click()
+# sleep(5)
+
+finally:
+    browser.quit()
+
+#browser.close()
